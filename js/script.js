4c7e51fc445cdac681a2363411cf59eb5a0ceb65
nsinssin <67975857+nsinssin@users.noreply.github.com>
2021-06-30 18:53:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        dots: false,
+        
+        speed: 500,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+      });
+        
+  });
+  
